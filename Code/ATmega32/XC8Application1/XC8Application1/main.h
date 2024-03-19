4e2926a187a0ad4fe7a5e8ade0bc851a2992e21f
Mahmoud Khaled <125205017+MahmoudKhaled25@users.noreply.github.com>
2024-03-19 06:28:13 +0200

    Add files via upload

---
+++ Code/ATmega32/XC8Application1/XC8Application1/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ * Created: 3/8/2024 10:23:10 AM
+ *  Author: Mahmoud Khaled
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*................... section:includes ....................*/
+#include "MCAL/DIO/Dio.h"
+
+/*............... section:macro declaration ...............*/
+
+/*.............. section:data type declaration ............*/
+
+/*.............. section:functions declaration ............*/
+
+
+#endif /* MAIN_H_ */
